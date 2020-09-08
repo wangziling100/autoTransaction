7c1cedd86c4cb92624415fc535a875ba6c75af0c
Bob Wang <wangziling1000@gmail.com>
2020-09-08 23:56:57 +0200

    init: main@@ init

--- forex/workspace/script/prepare_train_test_data_for_robot_0.py
+++
@@ -1,9 +0,0 @@
-from prepare_data import gen_online_transformed
-from get_robot_data import prepare_robot_data
-from train_robot import train_robot, save_norm_model
-
-name = 'EURUSD'
-gen_online_transformed()
-prepare_robot_data(name, data_length=600)
-save_norm_model(name)
-# train_robot(name, epoch=20, feature_bit=feature_bit)
