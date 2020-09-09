d1147c6273a47fedf525ec6fc5e6eb6def7fe2fe
Bob Wang <wangziling1000@gmail.com>
2020-09-10 01:33:33 +0200

    init: main @@ add main lambda

--- lambdas/main/transaction-main/app.js
+++ lambdas/main/transaction-main/app.js
@@ -1,12 +1,58 @@
-const k1 = process.env.k1
-exports.handler= (event, context) =>{
-    console.log('event: ', event)
-    console.log('env variable: ', k1)
+const puppeteer = require('puppeteer');
+
+
+exports.handler = async (event, context) =>{
+    const body = JSON.parse(event.body)
+    const command = body.command
+    switch(command){
+        case 'stop': return defaultResponse();
+        default: {
+            let browser = await getBrowser()
+            await browser.close()
+            break
+        } 
+    }
+    return defaultResponse()
+}
+
+exports.process = process
+async function process(driver){
+    console.log('here1')
+    await driver.get('www.google.de')
+    //const screenshot = await driver.takeScreenshot()
+    //console.log(screenshot, 'screenshot')
+}
+exports.getBrowser = getBrowser
+async function getBrowser(){
+    const browser = await puppeteer.launch({args:['--no-sandbox']});
+    return browser
+}
+
+exports.setResponse=setResponse
+function setResponse(options){
+    const statusCode = options.statusCode
+    delete options.statusCode
     const response = {
-        'statusCode': 200,
-        'body': JSON.stringify({
-            message: 'Hello World!'
-        })
+        'statusCode': statusCode,
+        'body': JSON.stringify(options),
+        'headers': {"Access-Control-Allow-Origin":"*", "Access-Control-Allow-Headers": "Content-Type",},
     }
     return response
 }
+
+exports.defaultResponse=defaultResponse
+function defaultResponse(){
+    const options = {
+        statusCode: 200,
+        message: 'succeed',
+    }
+    return setResponse(options)
+}
+/*
+const defaultEvent = {
+    body: JSON.stringify({
+        message: 'test'
+    })
+}
+exports.handler(defaultEvent)
+*/
--- lambdas/main/transaction-main/test/app.test.js
+++ lambdas/main/transaction-main/test/app.test.js
@@ -1,12 +1,31 @@
-const handler = require('../app').handler
-test('handler', ()=>{
-    const event = {
-        message1: 'message1'
-    }
-    expect(handler(event)).toEqual({
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello World!'
+const {handler, defaultResponse, getBrowser} = require('../app')
+describe('puppeteer browser', ()=>{
+    let browser
+    beforeAll(async()=>{
+        browser = await getBrowser()
+    }, 15000)
+    afterAll(async()=>{
+        await browser.close()
+    }, 15000)
+    test('it renders', async () => {
+        const page = await browser.newPage();
+        await page.goto('https://example.com');
+        await page.screenshot({path: 'example.png'});
+    })
+    
+}, 15000)
+
+describe('static method', ()=>{
+    test('response', ()=>{
+        expect(defaultResponse()).toEqual({
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'succeed'
+            }),
+            'headers': {
+                "Access-Control-Allow-Origin":"*", 
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
         })
     })
 })
